12e481a941a25ce9be3d9f8225826a1be9e9d6e6
spyder1211 <soichiro.yano.g@gmail.com>
2024-06-26 10:51:04 +0900

    chore: Update dependencies and add LangServeLayer to AWS SAM build configuration

--- app/main.py
+++ app/main.py
@@ -1,17 +1,54 @@
+#!/usr/bin/env python
+from typing import List
+
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
-import asyncio
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_aws import ChatBedrock
+from langserve import add_routes
 
-app = FastAPI()
+# 1. Create prompt template1
+system_template = "与えられた {特徴}　の人物に対して、考察しどんなキャラクターかを箇条書きで列挙してください:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '名前；{名前}、性別：{性別}、好きなもの：{好きなもの}、嫌いなもの：{嫌いなもの}')
+])
+
+# 1. Create prompt template2
+system_template = "与えられた{text}から、その人物がどのような個性を持っているか考慮して、その人らしく一言喋ってください:"
+prompt_template2 = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = ChatBedrock(
+    region_name='us-east-1',
+    model_id='anthropic.claude-3-haiku-20240307-v1:0',
+)
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser | prompt_template2 | model | parser
 
 
-async def streamer():
-    message = "This is streaming from Lambda!\n"
-    for char in message:
-        yield char
-        await asyncio.sleep(0.1)
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
 
+# 5. Adding chain route
+add_routes(
+    app,
+    chain,
+    path="/bedrock",
+)
 
-@app.get("/")
-async def index():
-    return StreamingResponse(streamer(), media_type="text/html")
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
